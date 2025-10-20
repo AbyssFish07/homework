ec3b4c20d93da816cd80ab3c3f6d9ac8e8954bae
AbyssFish07 <abyssfish123@outlook.com>
2025-10-20 22:34:59 +0800

    Create A1.9.cpp

---
+++ A1/A1.9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double height=0,wide=0;
+    cin>>height;
+    cin>>wide;
+    cout<<"Perimeter of the rectangle = "<<2*(height+wide)<<" inches"<<endl;
+    cout<<"Area of the rectangle = "<<height*wide<<" square inches"<<endl;
+
+}

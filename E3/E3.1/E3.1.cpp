d585b4be05adf9ad69e948488ef6e1425be5c6a8
AbyssFish07 <abyssfish123@outlook.com>
2025-10-28 17:47:05 +0800

    Add files via upload

---
+++ E3/E3.1/E3.1.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+int is(int number);
+
+int main()
+{
+    int count, i, sum;
+    
+    count = 0;
+    sum = 0;
+    for(i = 100; i <= 999; i++)
+    {
+        if(is(i) == 1)   
+        {
+            count++;
+            sum += i; 
+        }          
+    }
+    printf("count=%d, sum=%d\n", count, sum);
+    
+    return 0;    
+} 
+
+
+int is(int number)
+{
+    int a, b, c, sum;
+    a = number / 100;
+    b = (number / 10) % 10;  
+    c = number % 10;
+    sum = a + b + c;
+
+    if (sum == 5)
+        return 1;
+    else
+        return 0;
+}
